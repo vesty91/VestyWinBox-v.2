21521e4f9e60d4f617b63759c17e3a6be84ccaec
Vesty <vesty@example.com>
2025-08-17 06:14:13 +0200

    fix(build): remove stray /n+ artifacts; correct file:// path construction

--- src/components/UnifiedTile/UnifiedTile.tsx
+++ src/components/UnifiedTile/UnifiedTile.tsx
@@ -42,7 +42,9 @@ const UnifiedTile: React.FC<UnifiedTileProps> = ({
     p = p.replace(/^(assets\/)?assets\//, 'assets/')
     // Build absolute file URL: file:///C:/.../resources/dist/ + assets/...
     try {
-      const pathname = window.location.pathname.replace(/\\/g, '/')</n+      const distDir = pathname.replace(/\/index\.html.*$/i, '/')</n+      return `file://${distDir}${p}`
+      const pathname = window.location.pathname.replace(/\\/g, '/')
+      const distDir = pathname.replace(/\/index\.html.*$/i, '/')
+      return `file://${distDir}${p}`
     } catch {
       return p
     }
